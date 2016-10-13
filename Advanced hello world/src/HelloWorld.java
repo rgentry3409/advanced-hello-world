05f65ed03da75d431d4bf358d2bb5e7adb6ba0fa
rgentry3409 <rgentry3409@001LAB153010814.neisd.ad>
2016-10-13 15:49:08 -0500

    Initial commit

---
+++ Advanced hello world/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String myName;
+		myName = "Ryan Gentry";
+		int day;
+		day = 4;
+		int year;
+		year = 2001;
+		int rem;
+		rem = year%day;
+		System.out.println("My name is " + myName + ". My birthday is May " + day + ", "  + year + ". The remainder of " + day + " and " + year + " is " + rem + ".");
+		
+	}
+
+}
